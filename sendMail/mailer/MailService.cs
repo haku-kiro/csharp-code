38694a6a7fde515149b1c54a2457df6e9ce75abf
haku-kiro <botPrime@yahoo.com>
2018-01-07 01:39:53 +0200

    Adding send mail proj

---
+++ sendMail/mailer/MailService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Timers;
+
+namespace mailer
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer1;
+        int getCallType;
+        public Service1()
+        {
+            InitializeComponent();
+            int strTime = int.Parse(ConfigurationManager.AppSettings["callDuration"]);
+            getCallType = int.Parse(ConfigurationManager.AppSettings["CallType"]);
+            if (getCallType == 1)
+            {
+                ServiceLog.WriteErrorLog("the get call value is equal to 1");
+                timer1 = new Timer();
+                double inter = GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+            }
+            else
+            {
+                timer1 = new Timer();
+                timer1.Interval = strTime * 1000;
+                timer1.Elapsed += new ElapsedEventHandler(ServiceTimer_Tick);
+            }
+        }
+
+        private void ServiceTimer_Tick(object sender, ElapsedEventArgs e)
+        {
+            ServiceLog.WriteErrorLog("An email was supposed to be sent");
+            string Msg = "This is a test email from my test windows service";
+            ServiceLog.SendEmail("mdjcosta30@gmail.com","","","test",Msg);
+
+            if (getCallType == 1)
+            {
+                timer1.Stop();
+                System.Threading.Thread.Sleep(1000000);
+                SetTimer();
+            }
+        }
+
+        private double GetNextInterval()
+        {
+            var timeString = ConfigurationManager.AppSettings["StartTime"];
+            DateTime t = DateTime.Parse(timeString);
+            TimeSpan ts = new TimeSpan();
+            ts = t - DateTime.Now;
+            if (ts.TotalMilliseconds < 0)
+            {
+                ts = t.AddDays(1) - DateTime.Now; //inc the interval based on your req
+            }
+            return ts.TotalMilliseconds;
+        }
+
+        private void SetTimer()
+        {
+            try
+            {
+                double inter = (double)GetNextInterval();
+                timer1.Interval = inter;
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(ex.Message);
+                EventLog.WriteEntry(ex.StackTrace);
+                throw;
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.AutoReset = true;
+            timer1.Enabled = true;
+            ServiceLog.WriteErrorLog("Reporting service started");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.AutoReset = false;
+            timer1.Enabled = false;
+            ServiceLog.WriteErrorLog("Reporting service has stopped");
+        }
+    }
+}
---
+++ sendMail/mailer/ServiceLog.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Mail; //Mail message
+using System.Net;
+
+namespace mailer
+{
+    static class ServiceLog
+    {
+        #region Error logging methods
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + @"\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception exception)
+            {
+                EventLog x = new EventLog("Application");
+                x.WriteEntry("Your service log class has created an exception: " + exception.Message.ToString().Trim());
+                throw;
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + @"\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                EventLog x = new EventLog("Application");
+                x.WriteEntry("Your service log class has created an exception: " + ex.Message.ToString().Trim());
+                throw;
+            }
+        }
+        #endregion
+
+
+        #region Sending an email
+
+        public static void SendEmail(string toEmail, string cc, string bcc, string subject, string message)
+        {
+            string HostAdd = ConfigurationManager.AppSettings["Host"];
+            string FromEmailid = ConfigurationManager.AppSettings["FromMail"];
+            string Pass = ConfigurationManager.AppSettings["Password"];
+
+            //Creating the object of MailMessage //using system.net.mail
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(FromEmailid);
+            mailMessage.Subject = subject;
+            mailMessage.Body = message;
+            mailMessage.IsBodyHtml = true; //set bool based on the body of the email being html or not
+
+            //all the people you are sending to
+            string[] ToMuliID = toEmail.Split(',');
+            foreach (string email in ToMuliID)
+            {
+                mailMessage.To.Add(new MailAddress(email));
+            }
+
+
+
+            //TODO
+            ////all the cc (add error checking to check if string empty- cc and bcc)
+            //string[] CCMail = cc.Split(',');
+            //foreach (string email in CCMail)
+            //{
+            //    mailMessage.CC.Add(new MailAddress(email));
+            //}
+
+            ////all the bcc
+            //string[] BCCMail = bcc.Split(',');
+            //foreach (string email in BCCMail)
+            //{
+            //    mailMessage.Bcc.Add(new MailAddress(email));
+            //}
+
+
+
+            //network and security related creds
+
+            //using system.net
+
+            var smtp = new SmtpClient
+            {
+                Host = HostAdd,   
+                Port = int.Parse(ConfigurationManager.AppSettings["smtpPort"]),
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(FromEmailid, Pass)
+            };
+
+            smtp.Send(mailMessage);
+        }
+
+        #endregion
+
+    }
+}
